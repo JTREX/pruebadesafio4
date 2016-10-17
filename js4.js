da5d8389757cbdd16aa5fac7abd213a18477577b
jtrex <jatm_mr@hotmail.com>
2016-10-16 21:40:15 -0500

    first commit

---
+++ js4.js
@@ -0,0 +1,10 @@
+var b,h,l1,l2;
+h=prompt("Ingrese altura: ","6");
+b=prompt("Ingrese base: ","6");
+for(l1=0;l1<h;l1++)
+{	for(l2=0;l2<b;l2++)
+{
+document.write("*");
+}
+document.write("<br>");
+}
